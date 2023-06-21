e1af24b9b03710eff1f7a73c05fa0d8ef7549de8
Evan Robinson <erob7856@gmail.com>
2023-06-21 17:09:45 -0400

    Update About.svelte

--- src/components/About.svelte
+++ src/components/About.svelte
@@ -6,7 +6,7 @@
     <div class="card p-4 bg-dark-blue mt-4 rounded-lg w-full shadow-lg overflow-hidden">
         <p>
             I'm a current Computer Science student at the University of Florida. I love spending time typing, watching sports, and working out.
-            As someone who as always been passionate about technology and its potential to transform the world, I feel incredibly fortunate to be pursuing my degree
+            As someone who has always been passionate about technology and its potential to transform the world, I feel incredibly fortunate to be pursuing my degree
             in such an exciting and dynamic field. I enjoy keeping up with recent tech news by browsing forums such as <a href="https://news.ycombinator.com">Hacker News</a> and
             <a href="https://thehackernews.com/">The Hacker News</a>.
         </p>
